b4f0f4d97c270d331a99418cd04e98a3e5443012
andrx <andrx@gmail.com>
2022-10-21 23:52:18 +0300

    1

---
+++ DailyReport/Pages/Reports/FinalReport.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DailyReport.Pages.Reports
+{
+    public class FinalReportModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
