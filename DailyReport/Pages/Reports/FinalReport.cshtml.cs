cb5c5deeb117e2a3a427371ca4e24c84bcf98a44
andrx <andrx@gmail.com>
2022-10-23 15:41:17 +0300

    1

--- DailyReport/Pages/Reports/FinalReport.cshtml.cs
+++ DailyReport/Pages/Reports/FinalReport.cshtml.cs
@@ -1,19 +1,25 @@
 using DailyReport.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace DailyReport.Pages.Reports
 {
     public class FinalReportModel : PageModel
     {
+        FinalReport _finalReport;
+        ApplicationContext context;
+        public FinalReportModel(ApplicationContext db)
+        {
+            context = db;
+        }
+        public List<DepReport> Reports { get; private set; } = new();
+        DateTime actualDate = DateTime.Today.AddDays(-1);
+        
         public void OnGet()
         {
-            ApplicationContext context;
+            Reports = context.DepReports.AsNoTracking().ToList();
 
-            public FinalReportModel(ApplicationContext db)
-            {
-                context = db;
-            }
         }
     }
 }
