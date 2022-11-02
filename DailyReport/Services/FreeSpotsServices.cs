c9901e460e58e0d933caebbbab2622b6c34d713f
andrx <andrx@gmail.com>
2022-11-02 20:52:22 +0300

    1

---
+++ DailyReport/Services/FreeSpotsServices.cs
@@ -0,0 +1,32 @@
+﻿using DailyReport.Models;
+using DailyReport.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography;
+
+namespace DailyReport.Services
+{
+    public static class FreeSpotsServices
+    {
+        static FreeSpots _freeSpots;
+        public static FreeSpots CountSpots(List<DepReport> _dRep, DepartmentSpots _ds )
+        {
+#pragma warning disable CS8602 // Разыменование вероятной пустой ссылки.
+            _freeSpots.dep1 = _ds.dep1 + _ds.dep11Children - _dRep.Find(p => p.depNumber == 1).present - _dRep.Find(p => p.depNumber == 1).presentChildrens;
+            _freeSpots.dep11 = _ds.dep11 + _ds.dep11Children - _dRep.Find(p => p.depNumber == 11).present - _dRep.Find(p => p.depNumber == 11).presentChildrens;
+            //_freeSpots.dep2 = _ds.dep2 + _ds. - _dRep.Find(p => p.depNumber == 2).present - _dRep.Find(p => p.depNumber == 2).presentChildrens;
+            _freeSpots.dep3 = _ds.dep3 + _ds.dep3Children - _dRep.Find(p => p.depNumber == 3).present - _dRep.Find(p => p.depNumber == 3).presentChildrens;
+            _freeSpots.dep4 = _ds.dep4 + _ds.dep4Children - _dRep.Find(p => p.depNumber == 4).present - _dRep.Find(p => p.depNumber == 4).presentChildrens;
+            _freeSpots.dep5 = _ds.dep5 + _ds.dep5Children - _dRep.Find(p => p.depNumber == 5).present - _dRep.Find(p => p.depNumber == 5).presentChildrens;
+            _freeSpots.dep6 = _ds.dep6 + _ds.dep6Children - _dRep.Find(p => p.depNumber == 6).present - _dRep.Find(p => p.depNumber == 6).presentChildrens;
+            _freeSpots.dep7 = _ds.dep7 + _ds.dep7Children - _dRep.Find(p => p.depNumber == 7).present - _dRep.Find(p => p.depNumber == 7).presentChildrens;
+            _freeSpots.dep8 = _ds.dep8 + _ds.dep8Children - _dRep.Find(p => p.depNumber == 8).present - _dRep.Find(p => p.depNumber == 8).presentChildrens;
+            _freeSpots.dep90 = _ds.dep90 + _ds.dep90Children - _dRep.Find(p => p.depNumber == 90).present - _dRep.Find(p => p.depNumber == 90).presentChildrens;
+            _freeSpots.dep91 = _ds.dep91 + _ds.dep91Children - _dRep.Find(p => p.depNumber == 91).present - _dRep.Find(p => p.depNumber == 91).presentChildrens;
+
+#pragma warning restore CS8602 // Разыменование вероятной пустой ссылки.
+            return _freeSpots;
+        }
+    }
+}
