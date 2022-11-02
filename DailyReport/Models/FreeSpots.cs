a1dd1c1af18472c7ac0f91a0316902d49004e29a
andrx <andrx@gmail.com>
2022-11-02 23:33:53 +0300

    1

--- DailyReport/Models/FreeSpots.cs
+++ DailyReport/Models/FreeSpots.cs
@@ -15,5 +15,18 @@
         public int dep91 { get; set; }
         public int sum { get; set; }
         public int sumOC { get; set; }
+        public int dep1Children { get; set; }
+        public int dep11Children { get; set; }
+        public int dep2Children { get; set; }
+        public int dep3Children { get; set; }
+        public int dep4Children { get; set; }
+        public int dep5Children { get; set; }
+        public int dep6Children { get; set; }
+        public int dep7Children { get; set; }
+        public int dep8Children { get; set; }
+        public int dep90Children { get; set; }
+        public int dep91Children { get; set; }
+        public int sumChildren { get; set; }
+        public int sumOCChildren { get; set; }
     }
 }
