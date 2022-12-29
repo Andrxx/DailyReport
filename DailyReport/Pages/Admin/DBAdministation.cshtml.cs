de3b2a8bd86783de0e3290cacb6dac64abd5c47a
andrx <andrx@gmail.com>
2022-12-30 00:09:00 +0300

    1

---
+++ DailyReport/Pages/Admin/DBAdministation.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DailyReport.Pages.Admin
+{
+    public class DBAdministationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
