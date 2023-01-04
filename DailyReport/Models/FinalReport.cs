09e61c78a0dba780a4ded7a2a1018d81d1912ca0
andrx <andrx@gmail.com>
2023-01-05 01:54:33 +0300

    1

--- DailyReport/Models/FinalReport.cs
+++ DailyReport/Models/FinalReport.cs
@@ -60,8 +60,6 @@
         public int OKIChildren { get; set; }
         public int meningit { get; set; }
         public int meningitChildren { get; set; }
-        public int sepsis { get; set; }
-        public int sepsisChildren { get; set; }
         public int hepatit { get; set; }
         public int hepatitChildren { get; set; }
         public int HIV { get; set; }
@@ -75,40 +73,15 @@
         public int presentNonDaycare { get; set; }
         public int presentNonDaycareChildren { get; set; }
 
-        //уход
-        public int care { get; set; }
-        public int careDisodered { get; set; }
-        public int presentWithCare { get; set; }
-        public int presentWithCareChildren { get; set; }
-
-        //не госпитализированые пациенты
-        public int reject { get; set; }
-        public int rejectChildren { get; set; }
-        public int ambulance { get; set; }
-        public int ambulanceChildren { get; set; }
-        public int sendToMO { get; set; }
-        public int sendToMOChildren { get; set; }
-        public int sumAdults { get; set; }
-        public int sumChild { get; set; }
-        public int sumAll { get; set; }
-
-        /// <summary>
-        /// Считаем больных по отделениям
-        /// </summary>
-        /// <returns></returns>
         public int CountDiseases()
         {
-            int _summary = U071 + U072 + ORVI + grippe + pneumonia + OKI + meningit + hepatit + HIV + other + sepsis;
+            int _summary = U071 + U072 + ORVI + grippe + pneumonia + OKI + meningit + hepatit + HIV + other;
             return _summary;
         }
-        /// <summary>
-        /// Считаем больных детей по отделениям
-        /// </summary>
-        /// <returns></returns>
         public int CountDiseasesChildren()
         {
             int _summary = U071Children + U072Children + ORVIChildren + grippeChildren + pneumoniaChildren + OKIChildren + meningitChildren +
-                hepatitChildren + HIVCildren + otherChildren + sepsisChildren;
+                hepatitChildren + HIVCildren + otherChildren;
             return _summary;
         }
 
