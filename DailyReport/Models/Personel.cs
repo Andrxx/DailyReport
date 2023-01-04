947d36f3aea6f000f60be1302cde30aba6a819d0
andrx <andrx@gmail.com>
2023-01-05 01:06:11 +0300

    1

---
+++ DailyReport/Models/Personel.cs
@@ -0,0 +1,19 @@
+﻿namespace DailyReport.Models
+{
+    public class Personel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public Type PersType { get; set; }
+        public string Department { get; set; }
+    }
+
+    public enum Type
+    {
+        Doctor,
+        Nurse,
+        Operator,
+        Orderly
+    }
+}
