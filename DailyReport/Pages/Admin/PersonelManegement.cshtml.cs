6e89307766f3acf2146ae49bad6ba6775c19084d
andrx <andrx@gmail.com>
2023-01-04 21:04:21 +0300

    1

--- DailyReport/Pages/Admin/PersonelManegement.cshtml.cs
+++ DailyReport/Pages/Admin/PersonelManegement.cshtml.cs
@@ -1,12 +1,12 @@
-using DailyReport.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.EntityFrameworkCore;
 
 namespace DailyReport.Pages.Admin
 {
-    public class DBAdministrationModel : PageModel
+    public class PersonelManegementModel : PageModel
     {
-       
+        public void OnGet()
+        {
+        }
     }
 }
