94f00369cc84d0985d880da6532718d2b54f67ce
andrx <andrx@gmail.com>
2023-01-06 16:16:45 +0300

    1

--- DailyReport/Services/PersonelServices.cs
+++ DailyReport/Services/PersonelServices.cs
@@ -1,43 +1,9 @@
 ﻿using DailyReport.Models;
-using Microsoft.Build.Evaluation;
-using System.Numerics;
 
 namespace DailyReport.Services
 {
     public static class PersonelServices
     {
-        public static List<string> GetPType()
-        {
-            List<string> PType = new();
-            PType.Add("Врач");
-            PType.Add("Медсестра");
-            PType.Add("Оператор ПК");
-            PType.Add("Санитар");
-            return PType;
-        }
-
-        /// <summary>
-        /// получаем список отделений - фиксированный список из кода
-        /// </summary>
-        /// <returns></returns>
-        public static List<string> GetDepartment()
-        {
-            List<string> Department = new();
-            Department.Add("Приемное отделение");
-            Department.Add("1 отделение");
-            Department.Add("2 отделение");
-            Department.Add("3 отделение");
-            Department.Add("4 отделение");
-            Department.Add("5 отделение");
-            Department.Add("6 отделение");
-            Department.Add("7 отделение");
-            Department.Add("ОРИТ");
-            Department.Add("Рентгенологическое");
-            Department.Add("Дневной стационар");
-            Department.Add("Платные услуги");
-            return Department;
-        }
-
         public static void AddPersonel(Personel personel, ApplicationContext context)
         {
             context.Personels.Add(personel);
@@ -66,26 +32,6 @@ namespace DailyReport.Services
                 _personel.Department = personel.Department;
                 context.SaveChanges();
             }
-        }
-
-        /// <summary>
-        /// находим сотрудника по имени
-        /// </summary>
-        /// <param name="name"></param>
-        /// <param name="context"></param>
-        /// <returns></returns>
-        public static Personel GetPersonelByName(string name, ApplicationContext context)
-        {
-            return (from p in context.Personels
-                    where p.Name == name
-                    select p).FirstOrDefault();
-        }
-
-        public static Personel GetPersonelById(int id, ApplicationContext context)
-        {
-            return (from p in context.Personels
-                    where p.Id == id
-                    select p).FirstOrDefault();
         }
     }
 }
