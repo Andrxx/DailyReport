53ba657e15fb87d88c2afe478832ad60c824ecff
andrx <andrx@gmail.com>
2023-01-06 16:16:45 +0300

    1

--- DailyReport/Pages/Admin/PersonelManegement.cshtml.cs
+++ DailyReport/Pages/Admin/PersonelManegement.cshtml.cs
@@ -1,3 +1,4 @@
+using DailyReport.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,9 +6,18 @@ namespace DailyReport.Pages.Admin
 {
     public class PersonelManegementModel : PageModel
     {
+        ApplicationContext context;
+        public PersonelManegementModel(ApplicationContext db)
+        {
+            context = db;
+        }
+        public Personel personel, newPersonel = new Personel();
+        public List<Personel> personels = new List<Personel>();
+
 
         public void OnGet()
         {
+
         }
     }
 }
---
+++ DailyReport/Services/PersonelServices.cs
@@ -0,0 +1,37 @@
+﻿using DailyReport.Models;
+
+namespace DailyReport.Services
+{
+    public static class PersonelServices
+    {
+        public static void AddPersonel(Personel personel, ApplicationContext context)
+        {
+            context.Personels.Add(personel);
+            context.SaveChanges();
+        }
+
+        public static void DeletePersonel(int id, ApplicationContext context)
+        {
+            Personel personel = (from p in context.Personels
+                                        where (p.Id == id)
+                                        select p).FirstOrDefault();
+            if (personel != null) context.Personels.Remove(personel);
+            context.SaveChanges();
+        }
+
+        public static void UpdatePersonel(Personel personel, ApplicationContext context)
+        {
+            Personel _personel = (from p in context.Personels
+                                         where (p.Id == personel.Id)
+                                         select p).FirstOrDefault();
+            if (_personel != null)
+            {
+                _personel.Name = personel.Name;
+                _personel.PersType = personel.PersType;
+                _personel.Phone = personel.Phone;
+                _personel.Department = personel.Department;
+                context.SaveChanges();
+            }
+        }
+    }
+}
