43972f8b55d620c17fb446f2e7f73b3b06b0e566
andrx <andrx@gmail.com>
2023-01-09 01:47:21 +0300

    1

--- DailyReport/Pages/Admin/PersonelManegement.cshtml.cs
+++ DailyReport/Pages/Admin/PersonelManegement.cshtml.cs
@@ -1,6 +1,7 @@
 using DailyReport.Models;
+using DailyReport.Services;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.RazorPages; 
 
 namespace DailyReport.Pages.Admin
 {
@@ -13,11 +14,34 @@ namespace DailyReport.Pages.Admin
         }
         public Personel personel, newPersonel = new Personel();
         public List<Personel> personels = new List<Personel>();
-
-
+        public List<string> PType = PersonelServices.GetPType();
+        
         public void OnGet()
         {
+            personels = (from pers in context.Personels
+                        select pers).ToList();
+        }
 
+        public IActionResult OnPostSavePersonel(Personel newPersonel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction("Get");
+            }
+            PersonelServices.AddPersonel(newPersonel, context);
+            return RedirectToAction("Get");
+        }
+
+        public IActionResult OnPostDeletePersonel(int id)
+        {
+            PersonelServices.DeletePersonel(id, context);
+            return RedirectToAction("Get");
+        }
+
+        public IActionResult OnPostUpdatePersonel()
+        {
+            PersonelServices.UpdatePersonel(newPersonel, context);
+            return RedirectToAction("Get");
         }
     }
 }
