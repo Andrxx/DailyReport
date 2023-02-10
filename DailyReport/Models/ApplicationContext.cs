c435cc89bb4503b83ca32cbc21f39791da4ef1ca
andrx <andrx@gmail.com>
2023-02-11 00:38:00 +0300

    1

--- DailyReport/Models/ApplicationContext.cs
+++ DailyReport/Models/ApplicationContext.cs
@@ -5,22 +5,24 @@ namespace DailyReport.Models
 {
     public class ApplicationContext : DbContext
     {
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
+        {
+            //Database.EnsureDeleted();   // удаляем бд со старой схемой
+            //Database.EnsureCreated();   // создаем базу данных при первом обращении
+        }
         public DbSet<DepReport> DepReports { get; set; } = null!;
         public DbSet<FinalReport> FinalReports { get; set; } = null!;
         public DbSet<DutyDoc> DutyDocs { get; set; } = null!;
         public DbSet<OutcomingPatient> OutcomingPatients { get; set; } = null!;
         public DbSet<Personel> Personels { get; set; } = null!;
 
-        protected override void OnConfiguring(DbContextOptionsBuilder options)
-            => options.UseSqlite(@"Data Source="); 
+        //protected override void OnConfiguring(DbContextOptionsBuilder options)
+        //    => options.UseSqlite(@"Data Source=departmentsdb.db");
 
 
 
-        //public ApplicationContext(DbContextOptions<ApplicationContext> options)
-        //    : base(options)
-        //{
-        //    //Database.EnsureDeleted();   // удаляем бд со старой схемой
-        //    //Database.EnsureCreated();   // создаем базу данных при первом обращении
-        //}
+       
     }
 }
