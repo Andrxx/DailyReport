d961c2fa3bd02efcc90c97a779fbd7f60c5724bd
andrx <andrx@gmail.com>
2023-02-13 02:02:02 +0300

    1

---
+++ DailyReport/Migrations/20230212152020_CareAdded.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DailyReport.Migrations
+{
+    /// <inheritdoc />
+    public partial class CareAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "GLPS",
+                table: "DepReports",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "GLPSChildren",
+                table: "DepReports",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "care",
+                table: "DepReports",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "careDisodered",
+                table: "DepReports",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "sepsis",
+                table: "DepReports",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "sepsisChildren",
+                table: "DepReports",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "GLPS",
+                table: "DepReports");
+
+            migrationBuilder.DropColumn(
+                name: "GLPSChildren",
+                table: "DepReports");
+
+            migrationBuilder.DropColumn(
+                name: "care",
+                table: "DepReports");
+
+            migrationBuilder.DropColumn(
+                name: "careDisodered",
+                table: "DepReports");
+
+            migrationBuilder.DropColumn(
+                name: "sepsis",
+                table: "DepReports");
+
+            migrationBuilder.DropColumn(
+                name: "sepsisChildren",
+                table: "DepReports");
+        }
+    }
+}
---
+++ DailyReport/Migrations/20230212193301_CareAdded2.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DailyReport.Migrations
+{
+    /// <inheritdoc />
+    public partial class CareAdded2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "GLPS",
+                table: "DepReports");
+
+            migrationBuilder.DropColumn(
+                name: "GLPSChildren",
+                table: "DepReports");
+
+            migrationBuilder.AddColumn<int>(
+                name: "sepsis",
+                table: "FinalReports",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "sepsisChildren",
+                table: "FinalReports",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "sepsis",
+                table: "FinalReports");
+
+            migrationBuilder.DropColumn(
+                name: "sepsisChildren",
+                table: "FinalReports");
+
+            migrationBuilder.AddColumn<int>(
+                name: "GLPS",
+                table: "DepReports",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "GLPSChildren",
+                table: "DepReports",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
