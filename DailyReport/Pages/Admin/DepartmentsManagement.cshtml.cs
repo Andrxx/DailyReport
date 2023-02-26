6467ce0575511bc290458183f189544062174644
andrx <andrx@gmail.com>
2023-02-27 00:40:18 +0300

    1

--- DailyReport/Pages/Admin/DepartmentsManagement.cshtml.cs
+++ DailyReport/Pages/Admin/DepartmentsManagement.cshtml.cs
@@ -1,4 +1,5 @@
 using DailyReport.Models;
+using DailyReport.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -12,10 +13,38 @@ namespace DailyReport.Pages.Admin
             context = db;
         }
 
-        public List<Department> Departments { get; set; }
+        public List<Department> Departments = new();
         public Department department = new();
         public void OnGet()
         {
+            Departments = (from d in context.Departments
+                           select d).ToList();
+        }
+
+        public IActionResult OnPostSaveDepartment(Department department)
+        {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction("Get");
+            }
+            DepartmentServices.AddDepartment(department, context);
+            return RedirectToAction("Get");
+        }
+
+        public IActionResult OnPostDeleteDepartment(int id)
+        {
+            DepartmentServices.DeleteDepartment(id, context);
+            return RedirectToAction("Get");
+        }
+
+        public IActionResult OnPostUpdateDepartment(Department department)
+        {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction("Get");
+            }
+            DepartmentServices.UpdateDepartment(department, context);
+            return RedirectToAction("Get");
         }
     }
 }
