c0f16202a0a57787f9e79cc47236f3e7590a7ee9
andrx <andrx@gmail.com>
2023-02-26 17:26:09 +0300

    1

---
+++ DailyReport/Services/DepartmentServices.cs
@@ -0,0 +1,21 @@
+﻿namespace DailyReport.Services
+{
+    public static class DepartmentServices
+    {
+        public static List<string> Get()
+        {
+            List<string> PType = new();
+            PType.Add("Врач");
+            PType.Add("Медсестра");
+            PType.Add("Оператор ПК");
+            PType.Add("Санитар");
+            return PType;
+        }
+
+        //public static Enum DepNumbers { get; }
+        //{
+        //    first = 0
+        //    "Первое" = 1
+        //}
+    }
+}
