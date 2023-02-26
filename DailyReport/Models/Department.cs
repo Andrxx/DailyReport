35309fce347f03dd119d7fbb3b285a5b4d4bd479
andrx <andrx@gmail.com>
2023-02-26 15:58:58 +0300

    1

--- DailyReport/Models/Department.cs
+++ DailyReport/Models/Department.cs
@@ -4,8 +4,6 @@
     {
         public int Id { get; set; }
         public int Number { get; set; }
-        public int WardQuantity { get; set; }
-        public int AdultSpotsQuantity { get; set; }
-        public int ChildrenSpotsQuantity { get; set; }
+        
     }
 }
