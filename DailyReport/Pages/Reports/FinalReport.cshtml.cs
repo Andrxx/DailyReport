7c4eed3670c309ac38732744371338d290627264
andrx <andrx@gmail.com>
2023-03-02 02:10:04 +0300

    1

--- DailyReport/Pages/Reports/FinalReport.cshtml.cs
+++ DailyReport/Pages/Reports/FinalReport.cshtml.cs
@@ -47,10 +47,11 @@ namespace DailyReport.Pages.Reports
         public List<string> submitedFrom = OutPatientService.GetSubmitedFrom();
         public List<string> submitedTo = OutPatientService.GetSubmitedTo();
 
-        public void OnGet()
+        public void OnGet(double dateOffset = 0)
         {
+            actualDate = actualDate.AddDays(dateOffset);
             DateTime startTime = new DateTime(actualDate.Year, actualDate.Month, actualDate.Day, 7, 0, 0);
-            DateTime endTime = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, 6, 59, 59).AddDays(1);
+            DateTime endTime = new DateTime(actualDate.Year, actualDate.Month, actualDate.Day, 6, 59, 59).AddDays(1);
             if (actualDate.Hour < 7)
             {
                 startTime = startTime.AddDays(-1);
