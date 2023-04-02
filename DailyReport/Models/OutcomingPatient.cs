8721763ee65c07b8c65b8bd3d53c5c64fbed04d7
andrx <andrx@gmail.com>
2023-04-02 13:55:37 +0300

    1

--- DailyReport/Models/OutcomingPatient.cs
+++ DailyReport/Models/OutcomingPatient.cs
@@ -11,6 +11,6 @@
         public string? Diagnos { get; set; }
         public string? SubmitedFrom { get; set; }
         public string? SubmitedTo { get; set; }
-        public DateTime Date { get; set; } //= DateTime.Now.Date.AddDays(-1);
+        public DateTime Date { get; set; }
     }
 }
