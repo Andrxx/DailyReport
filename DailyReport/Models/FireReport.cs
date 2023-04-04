e1fedc5b9cab72ddc698c113e4ab31e7378e2d66
andrx <andrx@gmail.com>
2023-04-05 00:49:40 +0300

    lab security added

--- DailyReport/Models/FireReport.cs
+++ DailyReport/Models/FireReport.cs
@@ -39,7 +39,6 @@
                     {
                         name = "Четвертое отделение";
                         return name;
-
                     }
                 case 5:
                     {
@@ -65,13 +64,21 @@
                     {
                         name = "ОРИТ";
                         return name;
-
                     }
                 case 91:
                     {
                         name = "ОРИТ грязная зона";
                         return name;
-
+                    }
+                case 21:
+                    {
+                        name = "Лаборатория";
+                        return name;
+                    }
+                case 102:
+                    {
+                        name = "Охрана и тех.персонал";
+                        return name;
                     }
                 default:
                     {
