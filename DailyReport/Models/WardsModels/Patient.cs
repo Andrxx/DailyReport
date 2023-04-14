bd0ee7ade86072d34a550b19da0adffd3e96b742
andrx <andrx@gmail.com>
2023-04-15 02:56:07 +0300

    1

---
+++ DailyReport/Models/WardsModels/Patient.cs
@@ -0,0 +1,19 @@
+﻿namespace DailyReport.Models.WardsModels
+{
+    public class Patient
+    {
+        public int Id { get; set; }
+        public int WardNumber { get; set; }
+        public string Name { get; set; }
+        public string Male { get; set; }
+        public int Age { get; set; }
+        public int? AgeMonts { get; set; }
+        public string Diagnos { get; set; }
+        public DateTime HospitalisationDate { get; set; }
+        public bool HasRash { get; set; }
+        public bool IsUntochable { get; set; }
+        public bool IsDisodered { get; set; }
+        public bool HasCareRisk { get; set; }
+
+    }
+}
