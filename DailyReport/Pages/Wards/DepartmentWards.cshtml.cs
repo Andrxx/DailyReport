feb7d42cd4bec3547bfc0a94dfc1f029d5f06018
andrx <andrx@gmail.com>
2023-04-22 01:17:34 +0300

    1

--- DailyReport/Pages/Wards/DepartmentWards.cshtml.cs
+++ DailyReport/Pages/Wards/DepartmentWards.cshtml.cs
@@ -14,7 +14,7 @@ namespace DailyReport.Pages.Wards
             wards.Add(WardServices.CreateTestWard(1, 3, true, 3, false));
             wards.Add(WardServices.CreateTestWard(2, 3, true, 3, false));
             wards.Add(WardServices.CreateTestWard(3, 3, true, 4, false));
-            wards.Add(WardServices.CreateTestWard(4, 3, true, 4, false));
+            wards.Add(WardServices.CreateTestWard(4, 3, true, 4, true));
 
             patients.Add(PatientServices.CreateTestPatient(1, false, false, false, false));
             patients.Add(PatientServices.CreateTestPatient(2, false, false, false, false));
@@ -28,6 +28,22 @@ namespace DailyReport.Pages.Wards
             patients.Add(PatientServices.CreateTestPatient(10, false, false, false, false));
             patients.Add(PatientServices.CreateTestPatient(11, false, false, false, false));
 
+            //добавление пациентов в палаты
+            foreach(Ward ward in wards)
+            {
+                foreach (Patient patient in patients)
+                {
+                    if(patient.WardNumber == ward.Number) ward.PatientsInWard.Add(patient);
+                }
+            }
+
+            foreach (Ward ward in wards)
+            {
+                foreach (Patient patient in ward.PatientsInWard)
+                {
+                    if (patient.HasCareRisk || patient.HasRash || patient.IsUntochable) ward.CanPut = false;
+                }
+            }
         }
     }
 }
