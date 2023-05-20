1ec58f8b90f4aacfa9c97b050ed84e671709893e
andrx <andrx@gmail.com>
2023-05-21 00:09:16 +0300

    1

--- DailyReport/Pages/Wards/WardsSummary.cshtml.cs
+++ DailyReport/Pages/Wards/WardsSummary.cshtml.cs
@@ -2,6 +2,7 @@ using DailyReport.Models.WardsModels;
 using DailyReport.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using DailyReport.Services.WardServices;
 
 namespace DailyReport.Pages.Wards
 {
@@ -12,6 +13,8 @@ namespace DailyReport.Pages.Wards
         public List<Ward> wards { get; set; } = new();
         public Patient newPatient { get; set; } = new();
         public List<Patient> patients { get; set; } = new();
+        public List<string> sDepartments { get; set; } = new();
+        public List<int> iDepartments { get; set; } = new();
         public int departmentNumber;
         public WardsSummaryModel(ApplicationContext db)
         {
@@ -21,6 +24,9 @@ namespace DailyReport.Pages.Wards
 
         public void OnGet()
         {
+            //разбор по отделениям и палатам
+            sDepartments = WardServices.GetDepNmbersNames();
+            iDepartments = WardServices.GetDepNmbers();
         }
     }
 }
